a5a22315a29ea5521cb30c306b080e0b5893fba0
swapnil <swapnil.gadekar@in.tesco.com>
2017-08-06 22:17:21 +0530

    working example

--- src/App.js
+++ src/App.js
@@ -14,35 +14,50 @@ class App extends Component {
     let jobsPer = this.props.jobs/100;
     let val =1;
     path.forEach((index)=>{
-      let temp;
+      let temp,active;
+      switch (val) {
+        case 1:{temp=percentage;active='revenue';break;}
+        case 2:{temp=hoursPer;active='hours';break;}
+        case 3:{temp=jobsPer;active='jobs';break;}
+      }
       let length = index.getTotalLength();
-      console.log("testtts",index,);
+      console.log("testtts",index,val,active);
       // Clear any previous transition
       index.style.transition = index.style.WebkitTransition ='none';
       // Set up the starting positions
       index.style.strokeDasharray = length + ' ' + length;
-      index.style.strokeDashoffset = length * (1 - percentage);
+      index.style.strokeDashoffset = length * (1 - temp);
       let currentPathLength = length;
       setInterval(()=>{
-        if (currentPathLength <= length * (1 - percentage)) {
+        if (currentPathLength <= length * (1 - temp)) {
            clearInterval();
         return;
       }
       let percentText = 100 - Math.round(parseFloat(currentPathLength)/length * 100);
+      if(active=='revenue'){
+          this.setState({revenue:percentText});
+      }else if (active=='hours') {
+          this.setState({hours:percentText});
+      }else if(active=='jobs'){
+        this.setState({jobs:percentText});
+      }
       index.style.strokeDashoffset = currentPathLength;
       currentPathLength -= 2;
-      this.setState({revenue:percentText});
     });
+    val++;
     });
   }
   render() {
     return (
       <div className="figure">
       	<svg version="1.1" id="Layer_1" x="250px" y="250px" width="1000px" height="1000px">
-      		<path className="path" fill="none" stroke="#000000" strokeWidth="10"  strokeMiterlimit="10" d="M150,250a100,100 0 1,0 200,0a100,100 0 1,0 -200,0"/>
-          <path className="path" fill="none" stroke="#000000" strokeWidth="10" strokeMiterlimit="10" d="M400,250a100,100 0 1,0 200,0a100,100 0 1,0 -200,0"/>
-          <path className="path" fill="none" stroke="#000000" strokeWidth="10" strokeMiterlimit="10" d="M650,250a100,100 0 1,0 200,0a100,100 0 1,0 -200,0"/>
+      		<path className="path" fill="none" stroke="#FF9033" strokeWidth="12"  strokeMiterlimit="10" d="M150,250a100,100 0 1,0 200,0a100,100 0 1,0 -200,0"/>
+          <path className="path" fill="none" stroke="#57C5EC" strokeWidth="12" strokeMiterlimit="10" d="M400,250a100,100 0 1,0 200,0a100,100 0 1,0 -200,0"/>
+          <path className="path" fill="none" stroke="#EBDE1B" strokeWidth="12" strokeMiterlimit="10" d="M650,250a100,100 0 1,0 200,0a100,100 0 1,0 -200,0"/>
       	</svg>
+        <text x="0" y="15" fill="red">I love SVG!</text>
+        <text x="0" y="15" fill="red">I love SVG!</text>
+        <text x="0" y="15" fill="red">I love SVG!</text>
       	<p className="revenue">{this.state.revenue}%</p>
         <p className="hours">{this.state.hours}%</p>
         <p className="jobs">{this.state.jobs}%</p>
